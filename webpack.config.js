2c1ac765c3dbf026c1c0596fba749ee1b4534547
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2022-05-28 18:07:20 +0800

    build: init
    project init

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const { resolve } = require('path')
+module.exports = {
+  entry:'./src/index.js',
+  output:{
+    path:resolve(__dirname,'build'),
+    filename:'main.js'
+  },
+  mode:'development'
+}
