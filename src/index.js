3c40a055b3bddd8ed7209d39085b2c34ae2babdb
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2022-05-29 11:17:18 +0800

    build: load png support [flag2]

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 const temp = require('./temp.js')
+const tempUrl = require('./assets/temp.png')
+
+const logo = new Image()
+logo.src = tempUrl
+document.body.appendChild(logo)
 
 temp.hello(temp.v1)
 console.log('test')
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,16 @@ module.exports = {
     path: resolve(__dirname, 'build'),
     filename: 'js/bundle-[name]-[hash:5].js'
   },
-  mode: 'development'
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.png$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[name]-[hash:5][ext]'
+        }
+      }
+    ]
+  }
 }
